577c7e22f4da6b42f1d8bb7e7ed98da88e9d69e7
proudlyadvance <julia@juliamachado.com.br>
2023-11-10 13:24:50 -0300

    Fixed minor details in the search bar style

--- admin/components/dashboard/NavBar.tsx
+++ admin/components/dashboard/NavBar.tsx
@@ -1,13 +1,8 @@
-//TO-DO: Add search bar
-
 import React from 'react';
 import {
     Box,
     Toolbar,
     IconButton,
-    InputBase,
-    Badge,
-    Avatar,
     Typography,
     Menu,
     MenuItem,
@@ -15,9 +10,6 @@ import {
     InputAdornment,
 } from '@mui/material';
 import {
-    Search as SearchIcon,
-    Notifications as NotificationsIcon,
-    Apps as AppsIcon,
     ArrowDropDown as ArrowDropDownIcon,
 } from '@mui/icons-material';
 import Image from 'next/image';
@@ -39,9 +31,7 @@ function NavBar() {
 
     return (
         <Box position="static">
-            <Toolbar sx={{
-                width: '80vw'
-            }}>
+            <Toolbar sx={{ width: '80vw' }}>
                 <TextField fullWidth label="" id="fullWidth" placeholder="Search..."
                     InputProps={{
                         startAdornment: (
@@ -53,6 +43,8 @@ function NavBar() {
                             width: '300px',
                             height: '28px',
                             borderRadius: '4px',
+                            borderColor: '#E9ECEF',
+                            fontSize: '16px',
                             padding: '18px 16px',
                             marginBottom: '10px',
                             marginLeft: '10px'
@@ -61,10 +53,9 @@ function NavBar() {
                     }}
                 />
 
-                <div style={{ marginLeft: 'auto', display: 'flex', alignItems: 'center', gap: '10px'}}>
-                <Image alt='notification' src={iconNotification} />
-                <Image alt='message' src={iconMessage} />
-
+                <div style={{ marginLeft: 'auto', display: 'flex', alignItems: 'center', gap: '10px' }}>
+                    <Image alt='notification' src={iconNotification} />
+                    <Image alt='message' src={iconMessage} />
                     <div>
                         <IconButton
                             color="primary"
@@ -72,8 +63,7 @@ function NavBar() {
                             aria-controls="user-menu"
                             aria-haspopup="true"
                         >
-                            <Image alt="Alex" src={profilePic} style={{borderRadius: '50%'}}/>
-
+                            <Image alt="Alex" src={profilePic} style={{ borderRadius: '50%' }} />
                             <ArrowDropDownIcon />
                         </IconButton>
                         <Menu
@@ -83,7 +73,7 @@ function NavBar() {
                             onClose={handleMenuClose}
                         >
                             <MenuItem>
-                                <Image alt="Alex" src={profilePic} style={{borderRadius: '50%'}}/>
+                                <Image alt="Alex" src={profilePic} style={{ borderRadius: '50%' }} />
                                 <div>
                                     <Typography variant="body1">Alex</Typography>
                                     <Typography variant="body2" color="#8A92A6">
